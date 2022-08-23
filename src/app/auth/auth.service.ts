0049516f77651fe41359376220c12b62b6fb0380
Frank_Z <zxf_2719@163.com>
2022-08-23 23:38:44 +0800

    update login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from './storage.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,22 @@ export class AuthService {
   private loggedIn = false;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private jwtHelperService: JwtHelperService,
+    private storageService: StorageService
   ) {
     const token = localStorage.getItem('token');
     if (token) {
+      this.loggedIn = true;
+      const user = this.jwtHelperService.decodeToken(token).user;
+      console.log(this.jwtHelperService.decodeToken(token));
       // parse token and set user info
     }
   }
 
   login() {
     this.loggedIn = true;
+    this.storageService.setLocalStorage('token', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjaGFtYTFAY2hhbWEuY29tIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTY2MTI1ODg4OCwiZXhwIjoxNjkyNzk0ODg4fQ.fNjuH03FYEPlMujQS8hUnXrfvZGKbYFhUIJ8fTtSZI4');
     this.router.navigateByUrl("");
   }
 
